9b0bff66838735b6c4a86285cb98df03aa344427
a14ehsr <a14.ehsr@g.chuo-u.ac.jp>
2018-09-13 12:41:57 +0900

    自動コンパイルの修正

--- src/main/python/auto_compile/auto_compile.py
+++ src/main/python/auto_compile/auto_compile.py
@@ -9,18 +9,26 @@ def find_all_files(directory):
         for file in files:
             yield os.path.join(root, file)
 
-cmdf = open('src/main/resource/command_list/run_command_list.txt', mode='w')
+path = 'src/main/resource/command_list/'
+attack_cmd_file = open(path+'attack/attack_command_list.txt', mode='w')
+defence_cmd_file = open(path+'defence/defence_command_list.txt', mode='w')
 errf = open('src/main/resource/log/auto_compile/compile_err_list.txt', mode = 'w')
 cmdlist = []
 for file in find_all_files('./src/main/ai_program'):
 	root, ext = os.path.splitext(file)
 	directory, fname = os.path.split(root)
+
+	if fname.startswith("A_"):
+		output_file = attack_cmd_file
+	elif fname.startswith("D_"):
+		output_file = defence_cmd_file
+
 	if ext == '.java':
 		cmd = 'javac ' + file
 		err = subprocess.call(cmd, shell=True)
 		if err == 0:
 			runcmd = 'java -classpath '+ directory + ' ' + fname
-			cmdf.write(runcmd + '\n')
+			output_file.write(runcmd + '\n')
 			#f.write(runcmd+'\n')
 		else:
 			errf.write('COMPILE_ERROR '+file+'\n')
@@ -29,9 +37,9 @@ for file in find_all_files('./src/main/ai_program'):
 		err = subprocess.call(cmd, shell=True)
 		if err == 0:
 			runcmd = root
-			cmdf.write(runcmd + '\n')
+			output_file.write(runcmd + '\n')
 		else:
 			errf.write('COMPILE_ERROR '+file+'\n')
 	elif ext == '.py':
 		runcmd = 'python ' + file
-		cmdf.write(runcmd + '\n')
+		output_file.write(runcmd + '\n')
