4c482f739816ceeda71dc344107671a8ee493de6
a14ehsr <a14.ehsr@g.chuo-u.ac.jp>
2018-09-26 15:56:51 +0900

    サンプルプログラムの修正

--- sample_programs/c/defence/D_CSample.c
+++ sample_programs/c/defence/D_CSample.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 void init();
-int myNumber(int game,int round,int*** record);
-char *playerName = "playerCDefence";
+char *playerName = "D_CSample";
 
-
-int myNumber(int game,int round,int*** record){
-	if(round == 0){
-		return rand()%100 + 1;
-	}else{
-		return record[game][round - 1][0];
-	}
-}
 int main(void) {
 	srand((unsigned) time(NULL));
 	init();
@@ -23,9 +15,17 @@ int main(void) {
 			record[i][j] = malloc(sizeof(int ) * 2);
 		}
 	}
-	for(int i=0; i<100; i++){
-		for(int j=0; j<100; j++){
-			int myNum = myNumber(i,j,record);
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 100; j++) {
+			// ============== 書き換え箇所 ここから ===============
+			// myNumに数字を代入する．
+			// sampleでは最初のラウンドでランダムに入力し，それ以降は常に同じ数字とする．
+			int myNum = rand() % 100 + 1;
+			if (j != 0) {
+				myNum = record[i][j - 1][0];
+			}
+
+			// ============== 書き換え箇所 ここまで ===============
 			record[i][j][0] = myNum;
 			fprintf(stdout,"%d\n",myNum);
 			fflush(stdout);
