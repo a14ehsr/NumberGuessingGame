e9c673f297d848ad93d831a0babe7bb1c4d9d7fd
a14ehsr <a14.ehsr@g.chuo-u.ac.jp>
2018-10-15 17:02:56 +0900

    防御側指定の実行シェルスクリプトの実装

---
+++ vs_defence2.sh
@@ -0,0 +1,11 @@
+#!bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "指定された引数は$#個です。" 1>&2
+  echo "実行するには1個の引数が必要です。" 1>&2
+  exit 1
+fi
+
+echo $1
+
+java -classpath java/src ac.a14ehsr.platform.NumberGuessingGame -a "$1" -d "java -classpath java/src/ ac.a14ehsr.sample_ai.defence.D_RandomDeclare" -olevel 2
